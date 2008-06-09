b446c041c03e3b0888c70e097c70103afa0976ef
mbletzin <mbletzin@9223bfc7-3d92-4a1d-a1b8-4967b88859bd>
2008-06-09 13:40:50 +0000

    Added matlab scripts

---
+++ Operations Manager/Test Utilities/graph_logs.m
@@ -0,0 +1,70 @@
+clear all;
+close all;
+clc;
+
+s= '%s%s%s';
+s = [s,repmat('%f',1,96)];
+hs = repmat('%s',1,97);
+%orig_file = uigetfile('*.txt', 'Original Program Log','/Users/mbletzin/Work/MATLAB/');
+orig_file = 'C:\Documents and Settings\All Users\Desktop\Logs\May 14 LV 82 Build.txt';
+%new_file = uigetfile('*.txt', 'New Program Log','/Users/mbletzin/Work/MATLAB/');
+new_file = 'C:\Documents and Settings\All Users\Desktop\Logs\ContLog_2008_May_14_23_02_18.txt';
+fid = fopen(new_file, 'r');
+headers = textscan(fid,hs,1,'Delimiter','\t');
+raw = textscan(fid, s);
+[raw_c,raw_r] = size(raw);
+t = raw{1};
+t_c = size(t);
+new_dof = zeros(t_c(1),raw_r - 3);
+for i=4:raw_r
+    t = raw{i};
+    t_c = size(t);
+    
+    for rec=1:t_c(1)
+        new_dof(rec,i-3) = t(rec);
+    end
+end
+hdr = cell(95);
+for i=2:96
+    hdr(i-1) = headers{i};
+end
+        
+%sz = size(dof)
+%ndm = ndims(dof)
+fid = fopen(orig_file, 'r');
+headers = textscan(fid,hs,1,'Delimiter','\t');
+raw = textscan(fid, s);
+[raw_c,raw_r] = size(raw);
+t = raw{1};
+[t_c,t_r] = size(t);
+old_dof = zeros(t_c,raw_r-3);
+for i=4:raw_r
+    t = raw{i};
+    [t_c,t_r] = size(t);
+    
+    for rec=1:t_c
+        old_dof(rec,i-3) = t(rec);
+    end
+end
+
+stats = zeros(96,4);
+for i=1:96
+    stats(i,1) = mean(old_dof(:,i));
+    stats(i,3) = std(old_dof(:,i));
+    stats(i,2) = mean(new_dof(:,i));
+    stats(i,4) = std(new_dof(:,i));
+end
+for g = 1:5
+    strt = ((g - 1) * 18) + 1
+    nd = g * 18
+    subplot(2,3,g)
+    h = barh(1:18,stats(strt:nd,:),'group');
+    set(gca,'YTickLabel',hdr(strt:nd));
+    set(gca,'YTick',1:18);
+%    legend(h, 'Orig Mean', 'Orig Std Dev', 'New Mean', 'New Std Dev');
+end
+subplot(2,3,6)
+h = barh(1:6,stats(91:96,:),'group');
+%set(gca,'YTickLabel',hdr(91,96));
+set(gca,'YTick',1:6);
+legend(h, 'Orig Mean', 'New Mean','Orig Std Dev', 'New Std Dev');
---
+++ Operations Manager/Test Utilities/graph_volts.m
@@ -0,0 +1,80 @@
+clear all;
+close all;
+clc;
+
+ns = repmat('%f',1,54);
+hs = repmat('%s',1,54);
+root = 'C:\Documents and Settings\adminmbletzin\Desktop\Saved Logs\VoltsLog_2008_May';
+files = {'_18_01_14_54.txt'};
+last = 0;
+for f = 1:1
+    name = sprintf('%s%s',root,files{f});
+    fid = fopen(name, 'r');
+    clear headers;
+    headers = textscan(fid,hs,1,'Delimiter','\t');
+    raw = textscan(fid, ns);
+    fclose(fid);
+    [raw_c,raw_r] = size(raw);
+    t = raw{1};
+    t_c = size(t);
+    volts = zeros(t_c(1),raw_r);
+    for i=1:raw_r
+        t = raw{i};
+        t_c = size(t);
+    
+        for rec=1:t_c(1)
+            volts(rec,i) = t(rec);
+        end
+    end
+    t_c = size(volts);
+    if f == 1
+        allvolts = zeros((t_c(1)),t_c(2));
+    end
+    for i = 1:t_c(1)
+        for k = 1:t_c(2)
+            allvolts(last + i,k) = volts(i,k);
+        end
+    end
+    last = last + t_c(1)
+end
+
+headers(:)
+headers{1}
+headers{13}
+headers{19}
+headers{37}
+
+sz = size(allvolts)
+allvolts(:,19);
+grid on;
+plotdata = zeros(12*20,4);
+
+for i = 1:12*20
+    plotdata(i,1) = allvolts(i,1);
+    plotdata(i,2) = allvolts(i,13) - allvolts(i,1);
+    plotdata(i,3) = allvolts(i,19) - allvolts(i,1);
+    plotdata(i,4) = allvolts(i,37) - allvolts(i,1);
+end
+hdrs = cell(54);
+for h = 1:54
+    hdrs(h) = headers{h};
+end
+t_c = size(plotdata);
+xdata = [1:t_c(1)]
+xdata = xdata * 0.05
+h = plot(xdata, plotdata);
+legend(h,[ hdrs(1) hdrs(13) hdrs(19) hdrs(37)]);
+% for g = 1:5
+%     strt = ((g - 1) * 18) + 1
+%     nd = g * 18
+%     subplot(2,3,g)
+%     h = barh(1:18,stats(strt:nd,:),'group');
+%     set(gca,'YTickLabel',hdr(strt:nd));
+%     set(gca,'YTick',1:18);
+% %    legend(h, 'Orig Mean', 'Orig Std Dev', 'New Mean', 'New Std Dev');
+% end
+% subplot(2,3,6)
+% h = barh(1:6,stats(91:96,:),'group');
+% %set(gca,'YTickLabel',hdr(91,96));
+% set(gca,'YTick',1:6);
+% legend(h, 'Orig Mean', 'New Mean','Orig Std Dev', 'New Std Dev');
