403116b45a15a59f94eb9065120dc62fe930b946
mbletzin <mbletzin@9223bfc7-3d92-4a1d-a1b8-4967b88859bd>
2008-07-16 05:04:27 +0000

    test script

---
+++ Operations Manager/Test Utilities/testNtcpLink.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl 
+
+use IO::Socket::INET;
+
+
+my ($host, $port) = ("cee-zzul.cee.uiuc.edu","6342");
+#my ($host, $port) = ("192.168.1.101","5057");
+
+my $socket = new IO::Socket::INET(
+		PeerAddr => $host,
+		PeerPort => $port,
+		Proto     => 'tcp',
+	);
+
+die "Connection to $host:$port failed because $!" unless defined $socket;
+
+sendCommand("open-session dummySession");
+receiveCommand();
+
+sendCommand("set-parameter dummySetParam	nstep	0");
+receiveCommand();
+
+
+my $increment = "1.0";
+for my $i (1..500) {
+	($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
+	$year += 1900;
+	$month++;
+	print "$mon/$mday/$year";
+	sendCommand("propose\ttrans$year$month$mday$hour$min$sec.320\tMDL-00-01\tx\tdisplacement\t$increment");
+	receiveCommand();
+	sendCommand("execute\ttrans$year$month$mday$hour$min$sec.320");
+	receiveCommand();
+	sendCommand("get-control-point\tdummy\tMDL-00-01");
+	receiveCommand();
+	$increment = $increment eq "1.0" ? "-1.0" : "1.0";
+	sleep 3;
+}
+
+sendCommand("close-session	dummy");
+receiveCommand();
+
+close $socket;
+
+sub sendCommand {
+	my ($cmd) = @_;
+	print "Sending [$cmd]";
+	print $socket $cmd, "\n";
+
+}
+
+sub receiveCommand {
+	my $result = <$socket>;
+	chomp $result;
+	print "Received [$result]\n";
+}
